d54f9f389f591e72b3137df7c848fa6bd2a6056e
rick <github email address>
2020-05-11 17:04:24 -0400

    Added controller for projects

---
+++ backend/app/controllers/projects_controller.rb
@@ -0,0 +1,20 @@
+class ProjectsController < ApplicationController
+  def new
+  end
+  def index
+      
+  end
+  
+def show
+    
+end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
