a0382de6f77f92c05258add7e5b342bb1f848563
rick <github email address>
2020-05-11 17:01:23 -0400

    Added user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,4 +2,7 @@
 
 class User < ApplicationRecord
   has_secure_password
+  has_many :projects
+  validates_presence_of :email, on: :create, message: "can't be blank"
+  validates_presence_of :name, on: :create, message: "can't be blank"
 end
