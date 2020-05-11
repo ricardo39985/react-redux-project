85f1fa47fbe03b840882f74b6aa9e16b60810d37
rick <github email address>
2020-05-11 17:42:43 -0400

    Added Compnent Folder

---
+++ frontend/src/Component/LoginForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function LoginForm() {
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="" id="name"/><br/>
+                <label htmlFor="email">Email</label>
+                <input type="text" name="" id="email"/><br/>
+                <label htmlFor="password">Password</label>
+                <input type="text" name="" id="password"/><br/>
+
+                <input type="submit" name="" id=""/>
+
+            </form>
+        </div>
+    )
+}
