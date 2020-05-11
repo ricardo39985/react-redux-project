cb5fb0a2936c86375f5cd6225c75307a7127bba7
rick <github email address>
2020-05-11 17:02:14 -0400

    Added routes for: Login, Logout, signup, PROJECTS

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :users
+  resources :bugs
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+  resources :users, except: [:index] do
+    resources :projects
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
