918d5bbe47c9ba44a745a7476e2ffbbcfa9dc6af
rick <github email address>
2020-05-12 18:48:30 -0400

    connect to reduc store. add event handlers

--- frontend/src/Component/LoginForm.js
+++ frontend/src/Component/LoginForm.js
@@ -1,19 +1,59 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
+import { updateLoginForm } from "../actions/loginForm";
+import { login } from "../actions/currentUser";
 
-export default function LoginForm() {
-    return (
-        <div>
-            <form action="">
-                <label htmlFor="name">Name</label>
-                <input type="text" name="" id="name"/><br/>
-                <label htmlFor="email">Email</label>
-                <input type="text" name="" id="email"/><br/>
-                <label htmlFor="password">Password</label>
-                <input type="text" name="" id="password"/><br/>
+const LoginForm = ({ loginFormData, updateLoginForm, login}) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const forminfo = { ...loginFormData, [name]: value };
+    updateLoginForm(forminfo);
+  };
+  const handleSubmit =(event)=>{
+    event.preventDefault()
+    login(loginFormData)
 
-                <input type="submit" name="" id=""/>
+  }
+  return (
+    <div className="valign-wrapper">
+      <div
+        className="row
+      "
+      >
+        <div className="col s12 m12">
+          <div className="card blue-grey darken-1"></div>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={handleChange}
+              type="text"
+              name="email"
+              value={loginFormData.email}
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={handleChange}
+              type="text"
+              name="password"
+              value={loginFormData.password}
+            />
+            <br />
 
-            </form>
+            <input
+              style={{ borderRadius: "22px", width: "100%" }}
+              className="light-blue waves-effect waves btn"
+              type="submit"
+            />
+          </form>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    loginFormData: state.loginForm,
+  };
+};
+export default connect(mapStateToProps, { updateLoginForm, login })(LoginForm);
