71109b94055e3ea630f010701a2b06ca9b507b3a
rick <github email address>
2020-05-12 18:47:50 -0400

    Add imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import LoginForm from "./Component/LoginForm";
+import { NavBar } from "./Component/NavBar";
+import { getUser } from "./actions/currentUser";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+    this.props.getUser()
+  }
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <NavBar />
+          <LoginForm />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(null, {getUser})(App);
