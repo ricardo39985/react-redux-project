5a2c049d342c4a1ff8772700b1b3e1cbebe1fe2c
rick <github email address>
2020-05-13 11:50:25 -0400

    Updated Container

---
+++ frontend/src/Component/MainContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function MainContainer() {
+    return (
+        <div>
+            <h2>Side Bar</h2>
+            <ul>
+                <li>
+                    Add Project
+                </li>
+                <li>
+                    about
+                </li>
+            </ul>
+            <h2>List of all projects</h2>
+            <li>clickable</li>
+        </div>
+    )
+}
