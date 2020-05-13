7a7b7a1da0e8545aea815f710a47e9054cafdcde
rick <github email address>
2020-05-13 12:14:49 -0400

    Add new Reducers

--- frontend/src/reducers/loginForm .js
+++ frontend/src/reducers/loginForm .js
@@ -6,7 +6,8 @@ export default (state = initState, action) => {
   switch (action.type) {
     case "UPDATE_LOGIN_FORM":
       return action.formdata;
-
+      case "RESET_LOGIN_FORM":
+        return initState;
     default:
       return state;
   }
---
+++ frontend/src/reducers/projects.js
@@ -0,0 +1,10 @@
+export default (state =[], action)=>{
+    switch (action.type) {
+        case "SET_PROJECTS":
+            
+            break;
+    
+        default:
+            return state;
+    }
+}
