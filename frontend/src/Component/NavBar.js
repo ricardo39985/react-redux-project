47f9c381ff75d219197ca51d511a21e78307a47c
rick <github email address>
2020-05-13 12:11:18 -0400

    Templated Nav

--- frontend/src/Component/NavBar.js
+++ frontend/src/Component/NavBar.js
@@ -1,26 +1,32 @@
 import React from "react";
+import { connect } from "react-redux";
 
-export const NavBar = () => {
+const NavBar = ({currentUser}) => {
   return (
     <div>
       <nav>
         <div className="nav-wrapper">
           <a href="#" className="brand-logo right">
-            Logo
+            Bug Tracker
           </a>
-          <ul id="nav-mobile" className="left hide-on-med-and-down">
+          <ul id="nav-mobile" className="">
             <li>
               <a href="sass.html">Sass</a>
             </li>
-            <li>
-              <a href="badges.html">Components</a>
-            </li>
-            <li>
-              <a href="collapsible.html">JavaScript</a>
-            </li>
+            {!currentUser? <li>
+              <a href="/login">Login</a>
+            </li> : <li>
+              <a href="/logout">Logout</a>
+            </li> }
+            
+            
           </ul>
         </div>
       </nav>
     </div>
   );
 };
+const mapStateToProps =({currentUser})=>{
+return{currentUser}
+}
+export default connect(mapStateToProps)(NavBar)
