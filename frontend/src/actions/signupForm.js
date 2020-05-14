a0cbe281efb51cacbcd3c05de0abdf1e27f2e4d3
rick <github email address>
2020-05-13 21:59:42 -0400

    Add signup form

---
+++ frontend/src/actions/signupForm.js
@@ -0,0 +1,39 @@
+import { setCurrentUser } from "./currentUser";
+import { getProjects } from "./projects";
+
+export const updateSignUpFrom = (formdata) => {
+  return {
+    type: "UPDATE_SIGNUP_FORM",
+    formdata
+  };
+};
+export const resetSignupForm = () => {
+  return {
+    type: "RESET_SIGNUP_FORM",
+  };
+};
+
+export const signup = (credentials) => {
+    console.log(credentials);
+    return (dispatch) => {
+      return fetch("http://localhost:3000/signup", {
+          credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ credentials }),
+      })
+        .then((r) => r.json())
+        .then((d) => {
+          if (d.error) {
+            console.log("ERROR");
+          } else {
+            
+            dispatch(getProjects());
+            dispatch(setCurrentUser(d))
+            dispatch(resetSignupForm())
+          }
+        });
+    };
+  };
