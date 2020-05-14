cdb1f11f893a0656e824092ffd34541dde273870
rick <github email address>
2020-05-13 21:48:20 -0400

    Added new routes/Modified existing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,12 +2,11 @@
 
 Rails.application.routes.draw do
   resources :bugs
-  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get 'get_user', to: 'sessions#getCurrentUser'
-  resources :users, except: [:index] do
-    resources :projects
-  end
+  resources :users, except: [:index]
+  resources :projects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
