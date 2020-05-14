f76a08392b4349a6dc67f0afdf072c445bc768a7
rick <github email address>
2020-05-13 21:56:14 -0400

    Add new states to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,9 @@ import usersReducer from "../src/reducers/users";
 import thunk from "redux-thunk";
 import currentUser from "../src/reducers/currentUser";
 import loginForm from '../src/reducers/loginForm '
+import projects from '../src/reducers/projects'
+import signupForm from '../src/reducers/signupForm'
+import newProjectForm from '../src/reducers/projectForm'
 //For Redux Chrome extension
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -10,5 +13,8 @@ const reducer = combineReducers({
   users: usersReducer,
   currentUser,
   loginForm,
+  projects,
+  signupForm,
+  newProjectForm
 });
 export default createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
