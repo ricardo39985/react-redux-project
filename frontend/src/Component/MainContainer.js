84623e76cd1fd04292a9a4ba9983e3acb6bbd1df
rick <github email address>
2020-05-13 21:51:33 -0400

    Add new imports

--- frontend/src/Component/MainContainer.js
+++ frontend/src/Component/MainContainer.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import LoginForm from './LoginForm'
+import ProjectsContainer  from './ProjectsContainer'
 
 export default function MainContainer() {
     return (
         <div>
+            {/* <LoginForm/> */}
+            <ProjectsContainer/>
             <h2>Side Bar</h2>
             <ul>
                 <li>
