ee5a2fb72573a4cd67d62c9c729819ab52f4e079
rick <github email address>
2020-05-13 22:35:24 -0400

    Add async action for subm itting new project

--- frontend/src/actions/projectForm.js
+++ frontend/src/actions/projectForm.js
@@ -1,6 +1,29 @@
+import { getProjects } from "./projects";
+
 export const updateNewProjectForm = (formdata) => {
     return {
       type: "UPDATE_NEW_PROJECT_FORM",
       formdata
     };
   };
+
+  export const submitNewProject = (projectInfo)=>{
+    return dispatch =>{
+      return fetch("http://localhost:3000/projects", {
+        credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ projectInfo })
+    })
+    .then(resp =>resp.json())
+    .then(data=>{
+      if (data.error) {
+        
+      } else {
+        dispatch(getProjects())
+      }
+    })
+    }
+  }
