9726785ab3aef3b2ef937f01de06dcb7cc05f381
rick <github email address>
2020-05-13 21:55:29 -0400

    reformat code

--- frontend/src/reducers/projects.js
+++ frontend/src/reducers/projects.js
@@ -1,8 +1,7 @@
 export default (state =[], action)=>{
     switch (action.type) {
         case "SET_PROJECTS":
-            
-            break;
+            return action.projects
     
         default:
             return state;
