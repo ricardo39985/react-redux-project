f68e0dba59a3dd220a14c8f4ca7ab86bca68140c
rick <github email address>
2020-05-13 21:44:41 -0400

    Updated index action for projects

--- backend/app/controllers/projects_controller.rb
+++ backend/app/controllers/projects_controller.rb
@@ -1,20 +1,24 @@
+# frozen_string_literal: true
+
 class ProjectsController < ApplicationController
-  def new
-  end
+  def new; end
+
   def index
+    if logged_in?
+      render json: current_user.projects
       
+    else
+      render json: {error: "Not Logged in."}
+    end
   end
-  
-def show
-    
+
+  def show; end
+
+  def create; end
+
+  def update; end
+
+  def delete; end
 end
 
-  def create
-  end
 
-  def update
-  end
-
-  def delete
-  end
-end
