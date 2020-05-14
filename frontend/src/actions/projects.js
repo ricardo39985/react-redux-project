63bca1025f32b3f7413870f4bbd7ef74060a24ba
rick <github email address>
2020-05-13 21:54:06 -0400

    formating

--- frontend/src/actions/projects.js
+++ frontend/src/actions/projects.js
@@ -1,6 +1,23 @@
-export const setProjects = projects =>{
-    return{
-        type: "SET_PROJECTS",
-        projects
-    }
-}
+export const setProjects = (projects) => {
+  return {
+    type: "SET_PROJECTS",
+    projects,
+  };
+};
+
+export const getProjects = () => {
+  return (dispatch) => {
+    return fetch("http://localhost:3000/projects", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((r) => r.json())
+      .then((d) => {
+        // console.log(d);
+        dispatch(setProjects(d));
+      });
+  };
+};
