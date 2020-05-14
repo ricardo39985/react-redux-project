6faa1947d23e9c1f0d5e35495e4fd0cc8f9b6647
rick <github email address>
2020-05-13 21:50:26 -0400

    Added new imports, add routing, modify componentDidMount

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,41 @@
 import React, { Component } from "react";
 import "./App.css";
 import LoginForm from "./Component/LoginForm";
-import { NavBar } from "./Component/NavBar";
+import NavBar from "./Component/NavBar";
 import { getUser } from "./actions/currentUser";
 import { connect } from "react-redux";
-import Logout  from "./Component/Logout";
+import Logout from "./Component/Logout";
+import MainContainer from "./Component/MainContainer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import  SignUpForm  from "./Component/SignUpForm";
+import NewProjectForm from "./Component/NewProjectForm";
 
 class App extends Component {
-  componentDidMount(){
-    this.props.getUser()
+  componentDidMount() {
+    this.props.getUser();
   }
   render() {
     return (
       <div>
         <div className="App">
           <NavBar />
-          {!this.props.currentUser ? <LoginForm /> : <Logout/>  }
-          
-         
+          <Router>
+            <Route exact path="/login" component={LoginForm} />
+            <Route exact path="/dashboard" component={MainContainer} />
+            <Route exact path="/signup" component={SignUpForm} />
+            <Route exact path="/projects/new" component={NewProjectForm} />
+
+            {/* {!this.props.currentUser ? <LoginForm /> : <Logout/>  } */}
+            {/* <MainContainer/>  */}
+          </Router>
         </div>
       </div>
     );
   }
 }
-const mapStateToProps =(state)=>{
-  return{
-    currentUser: state.currentUser
-  }
-}
-export default connect(mapStateToProps, {getUser})(App);
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.currentUser,
+  };
+};
+export default connect(mapStateToProps, { getUser })(App);
