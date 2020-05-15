85e5854873b72742687c9da0d45b4beb439b5f9f
rick <github email address>
2020-05-15 02:46:13 -0400

    Adde new action, modified existing action

--- frontend/src/actions/projectForm.js
+++ frontend/src/actions/projectForm.js
@@ -1,29 +1,34 @@
 import { getProjects } from "./projects";
 
 export const updateNewProjectForm = (formdata) => {
-    return {
-      type: "UPDATE_NEW_PROJECT_FORM",
-      formdata
-    };
+  return {
+    type: "UPDATE_NEW_PROJECT_FORM",
+    formdata,
   };
+};
+export const resetProjectForm = () => {
+  return {
+    type: "RESET_NEW_PROJECT_FORM",
+  };
+};
 
-  export const submitNewProject = (projectInfo)=>{
-    return dispatch =>{
-      return fetch("http://localhost:3000/projects", {
-        credentials: "include",
+export const submitNewProject = (projectInfo) => {
+  return (dispatch) => {
+    return fetch("http://localhost:3000/projects", {
+      credentials: "include",
       method: "POST",
       headers: {
         "Content-type": "application/json",
       },
-      body: JSON.stringify({ projectInfo })
+      body: JSON.stringify({ projectInfo }),
     })
-    .then(resp =>resp.json())
-    .then(data=>{
-      if (data.error) {
-        
-      } else {
-        dispatch(getProjects())
-      }
-    })
-    }
-  }
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (data.error) {
+        } else {
+          dispatch(getProjects());
+          dispatch(resetProjectForm());
+        }
+      });
+  };
+};
