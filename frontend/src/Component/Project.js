4bf7be088b8c453de3f750fba8e80f9d24a55cfe
rick <github email address>
2020-05-18 02:31:17 -0400

    update Styling

--- frontend/src/Component/Project.js
+++ frontend/src/Component/Project.js
@@ -4,17 +4,17 @@ import {deleteProjectDB} from '../actions/projects'
 const ProjectCard = ({ project, deleteProjectDB }) => {
   const bstyle = { borderRadius: "18px", margin: "5px" };
   return (
-    <div className="valign-wrapper center">
-      <div class="row">
-        <div class="col s12 m8">
+    <div style ={{width: "400px;", margin: "10px",height: "200px"}}className="left">
+      <div class="center">
+        <div class="col s10 m2">
           <div class="card white darken-2">
-            <div class="card-content black-text">
-              <span class="card-title">{project.name}</span>
+            <div class="">
+              <span class="card-title ">{project.name}</span>
               <p className="flow">
                Project is currently in: {project.state}
               </p>
             </div>
-            <div class="card-action">
+            <div class="">
               <a href="href= {`/projects/${project.id}`}">
                 <button
                   className="waves-effect waves-light blue  btn"
@@ -31,14 +31,14 @@ const ProjectCard = ({ project, deleteProjectDB }) => {
                   Delete
                 </button>
               </a>
-              <a href="href= {`/projects/${project.id}`}">
+              {/* <a href="href= {`/projects/${project.id}`}">
                 <button
                   className="waves-effect waves-light  deep-orange accent-4 btn"
                   style={bstyle}
                 >
                   {project.state}
                 </button>
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
